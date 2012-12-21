eed6a9c3fb1de3a79b02cacc568f67406097c80d
unknown <C:\Users\DLX\AppData\Roaming\The Bat! Pwd>
2012-12-21 13:10:37 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '33ca905d9d6784753bb7f5da44686920febaa4b51df29cc9983b489a00cc91effefe4faeb892b3ba999272a3d053363e806621a96a19f113263c8648a1791c72'
